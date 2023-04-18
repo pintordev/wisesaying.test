9c1ded7cf06fa812d76c6560d708e50ed8d5ff1d
pintordev <pintor.dev@gmail.com>
2023-04-18 19:33:26 +0900

    stage #5 before refactoring

--- src/main/java/com/application/App.java
+++ src/main/java/com/application/App.java
@@ -1,19 +1,23 @@
 package com.application;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class App {
 
     private final Scanner scanner;
+    private ArrayList<WiseSaying> wiseSayingList;
 
     public App(Scanner scanner) {
         this.scanner = scanner;
+        this.wiseSayingList = new ArrayList<>();
     }
 
     public void run() {
         int id = 0;
         System.out.println("== Wise Saying Application ==");
         System.out.println("1. post");
+        System.out.println("2. list");
         System.out.println("0. terminate");
 
         while(true) {
@@ -33,9 +37,28 @@ public class App {
 
                 System.out.println("=".repeat(31));
                 System.out.printf("#%d wise saying has been posted.\n", ++id);
+
+                WiseSaying wiseSaying = new WiseSaying(id, author, content);
+                this.wiseSayingList.add(wiseSaying);
+
+            } else if (command.equals("list")) {
+                System.out.println("=".repeat(31));
+                if (this.wiseSayingList.isEmpty()) {
+                    System.out.println("There is no list in this application.");
+                    continue;
+                }
+                System.out.println("Id / Author / Content");
+                System.out.println("-".repeat(27));
+
+                for (int i = this.wiseSayingList.size() - 1; i >= 0; i--) {
+                    WiseSaying wiseSaying = this.wiseSayingList.get(i);
+                    System.out.println(wiseSaying.getId() + " / " + wiseSaying.getAuthor() + " / " + wiseSaying.getContent());
+                }
+
             } else if (command.equals("help")) {
                 System.out.println("=".repeat(31));
                 System.out.println("1. post");
+                System.out.println("2. list");
                 System.out.println("0. terminate");
             } else {
                 System.out.println("Please enter the valid command.");
